4212a7a46dd45ef4761f71e3c72351e9595c9830
ΛLIRΞZΛ <72320688+alyrezo@users.noreply.github.com>
2022-12-15 01:38:23 +0330

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,9 @@ for i in profiles:
     try:
         results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8', errors="backslashreplace").split('\n')
         results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
-try:
-        results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8', errors="backslashreplace").split('\n')
-        results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
         try:
             print ("{:<30}|  {:<}".format(i, results[0]))
         except IndexError:
             print ("{:<30}|  {:<}".format(i, ""))
-except subprocess.CalledProcessError:
+    except subprocess.CalledProcessError:
         print ("{:<30}|  {:<}".format(i, "ENCODING ERROR"))
